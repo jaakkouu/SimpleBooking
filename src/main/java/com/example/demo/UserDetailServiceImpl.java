2cf3217b9723df9b1b69f0f6b0e63da3ba7f87df
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2019-10-06 14:17:27 +0300

    added login ability, added bootstrap, added frontpage, added threee users

---
+++ src/main/java/com/example/demo/UserDetailServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.User;
+import com.example.demo.dao.UserRepository;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService  {
+	private final UserRepository repository;
+
+	@Autowired
+	public UserDetailServiceImpl(UserRepository userRepository) {
+		this.repository = userRepository;
+	}
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {   
+    	User currentUser = repository.findByUsername(username);
+        UserDetails user = new org.springframework.security.core.userdetails.User(username, currentUser.getPasswordHash(), null);
+        return user;
+    }   
+} 
