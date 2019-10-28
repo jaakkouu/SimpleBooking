294bb5b26d9739c6e9f3edd2bb4c8170ee261b49
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2019-10-28 21:52:23 +0200

    Update schema.sql
    added constraints

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users(
   id SERIAL PRIMARY KEY,
-  username varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
+  username varchar(255) UNIQUE NOT NULL,
+  email varchar(255) UNIQUE NOT NULL,
   password varchar(255) NOT NULL,
   enabled INTEGER DEFAULT 0,
   created_at timestamp,
@@ -11,15 +11,15 @@ CREATE TABLE IF NOT EXISTS users(
 
 CREATE TABLE IF NOT EXISTS authorities(
   id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
-  username varchar(255) NOT NULL,
+  user_id INTEGER UNIQUE REFERENCES users(id),
+  username varchar(255) UNIQUE NOT NULL,
   authority varchar(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS places(
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id),
-  name varchar(255) NOT NULL,
+  name varchar(255) UNIQUE NOT NULL,
   address varchar(255) NOT NULL,
   large_description varchar(1024) NOT NULL,
   small_description varchar(100) NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS places(
 CREATE TABLE IF NOT EXISTS place_urls(
   id SERIAL PRIMARY KEY,
   place_id INTEGER REFERENCES places(id),
-  url varchar(255) NOT NULL,
+  url varchar(255) UNIQUE NOT NULL,
   created_at timestamp,
   modified_at timestamp,
   removed_at timestamp
@@ -41,9 +41,9 @@ CREATE TABLE IF NOT EXISTS bookings(
   id SERIAL PRIMARY KEY,
   place_id INTEGER REFERENCES places(id),
   name varchar(255) NOT NULL,
-  phonenumber varchar(255) NOT NULL,
+  phonenumber varchar(255) UNIQUE NOT NULL,
   reservation_date date NOT NULL,
-  receipt_number varchar(25) NOT NULL,
+  receipt_number varchar(25) UNIQUE NOT NULL,
   created_at timestamp,
   modified_at timestamp,
   removed_at timestamp
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS bookings(
 
 CREATE TABLE IF NOT EXISTS companies(
   id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
+  user_id INTEGER UNIQUE REFERENCES users(id),
   name varchar(255),
   address varchar(255),
   modified_at timestamp
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS companies(
 
 CREATE TABLE IF NOT EXISTS contacts(
   id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
+  user_id INTEGER UNIQUE REFERENCES users(id),
   first_name varchar(255),
   last_name varchar(255),
   address varchar(255),
